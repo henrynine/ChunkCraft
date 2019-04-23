450c0c679fc1498c74e263781fbe073d43ebab0e
htm29 <htm29@cornell.edu>
2019-04-23 15:49:48 -0400

    Make clean, add .gitignore

--- _build/colors.mli
+++
@@ -1,7 +0,0 @@
-open ANSITerminal
-
-val brown : ANSITerminal.style
-val on_brown : ANSITerminal.style
-
-val gray : ANSITerminal.style
-val on_gray : ANSITerminal.style
--- _build/control.mli
+++
@@ -1,3 +0,0 @@
-open State
-(** Handle [command]. *)
-val handle_command : State.map -> char -> bool ref -> State.map
